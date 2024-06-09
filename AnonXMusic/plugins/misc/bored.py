15c528ce17b51887e4a121fb999d6e572176167d
『ʟᴏᴄᴏ』﹤/3 『ᴘɪʟᴏᴛ』 <155711756+LOCO-PILOT@users.noreply.github.com>
2024-06-09 22:03:57 +0530

    Create bored.py

---
+++ AnonXMusic/plugins/misc/bored.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, filters
+import requests
+from AnonXMusic import app
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+EVAA = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+    ],
+]
+
+# URL for the Bored API
+bored_api_url = "https://apis.scrimba.com/bored/api/activity"
+
+
+# Function to handle /bored command
+@app.on_message(filters.command("bored", prefixes="/"))
+async def bored_command(client, message):
+    # Fetch a random activity from the Bored API
+    response = requests.get(bored_api_url)
+    if response.status_code == 200:
+        data = response.json()
+        activity = data.get("activity")
+        if activity:
+            # Send the activity to the user who triggered the command
+            await message.reply(f"❖ ғᴇᴇʟɪɴɢ ʙᴏʀᴇᴅ ? ʜᴏᴡ ᴀʙᴏᴜᴛ ⏤͟͟͞͞★\n\n● `{activity}`\n\n❖ ғᴇᴇʟɪɴɢ ʙʏ ➥ [๛ɴ ʏ ᴋ ᴀ ᴀ ♡゙](htps://t.me/nykaaxbot)", reply_markup=InlineKeyboardMarkup(EVAA),)
+        else:
+            await message.reply("✦ ɴᴏ ᴀᴄᴛɪᴠɪᴛʏ ғᴏᴜɴᴅ.")
+    else:
+        await message.reply("✦ ғᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ ᴀᴄᴛɪᴠɪᴛʏ.")
+
