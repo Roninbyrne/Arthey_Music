1e9e90dc0be67c413dc8e6c4ef444a592e23f7fa
『ʟᴏᴄᴏ』﹤/3 『ᴘɪʟᴏᴛ』 <155711756+LOCO-PILOT@users.noreply.github.com>
2024-06-09 22:26:39 +0530

    Create dicegame.py

---
+++ AnonXMusic/plugins/tools/dicegame.py
@@ -0,0 +1,49 @@
+from pyrogram import Client, enums, filters
+import asyncio
+from AnonXMusic import app
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.handlers import MessageHandler
+
+#####
+EVAA = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/avishaxbot?startgroup=true"),
+    ],
+]
+
+####
+
+@app.on_message(filters.command("dice"))
+async def dice(bot, message):
+    x=await bot.send_dice(message.chat.id)
+    m=x.dice.value
+    await message.reply_text(f"⬤ ʜᴇʏ ʙᴀʙʏ ➥ {message.from_user.mention}\n⬤ ʏᴏᴜʀ sᴄᴏʀᴇ ɪs ➥ {m}", reply_markup=InlineKeyboardMarkup(EVAA) ,quote=True)
+  
+@app.on_message(filters.command("dart"))
+async def dart(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎯")
+    m=x.dice.value
+    await message.reply_text(f"⬤ ʜᴇʏ ʙᴀʙʏ ➥ {message.from_user.mention}\n⬤ ʏᴏᴜʀ sᴄᴏʀᴇ ɪs ➥ {m}", reply_markup=InlineKeyboardMarkup(EVAA),quote=True)
+
+@app.on_message(filters.command("basket"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🏀")
+    m=x.dice.value
+    await message.reply_text(f"⬤ ʜᴇʏ ʙᴀʙʏ ➥ {message.from_user.mention}\n⬤ ʏᴏᴜʀ sᴄᴏʀᴇ ɪs ➥ {m}",reply_markup=InlineKeyboardMarkup(EVAA), quote=True)
+@app.on_message(filters.command("jackpot"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎰")
+    m=x.dice.value
+    await message.reply_text(f"⬤ ʜᴇʏ ʙᴀʙʏ ➥ {message.from_user.mention}\n⬤ ʏᴏᴜʀ sᴄᴏʀᴇ ɪs ➥ {m}",reply_markup=InlineKeyboardMarkup(EVAA), quote=True)
+@app.on_message(filters.command("ball"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎳")
+    m=x.dice.value
+    await message.reply_text(f"⬤ ʜᴇʏ ʙᴀʙʏ ➥ {message.from_user.mention}\n⬤ ʏᴏᴜʀ sᴄᴏʀᴇ ɪs ➥ {m}",reply_markup=InlineKeyboardMarkup(EVAA), quote=True)
+@app.on_message(filters.command("football"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "⚽")
+    m=x.dice.value
+    await message.reply_text(f"⬤ ʜᴇʏ ʙᴀʙʏ ➥ {message.from_user.mention}\n⬤ ʏᴏᴜʀ sᴄᴏʀᴇ ɪs ➥ {m}",reply_markup=InlineKeyboardMarkup(EVAA), quote=True)
+
+#####
