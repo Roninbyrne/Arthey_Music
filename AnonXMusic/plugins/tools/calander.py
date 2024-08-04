5a58007d9a941bd710975e862f5244c24b6fc7f8
Ronin <bhabyaprakash01@gmail.com>
2024-08-04 16:08:39 +0530

    Update calander.py

--- AnonXMusic/plugins/tools/calander.py
+++ AnonXMusic/plugins/tools/calander.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/the_friendz"),
+        InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/bsbaaass"),
     ],
 ]
 
