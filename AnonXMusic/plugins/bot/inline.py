105aa3728e457f9ee908b487548a598023379604
Ronin <bhabyaprakash01@gmail.com>
2024-08-04 15:49:34 +0530

    Update inline.py

--- AnonXMusic/plugins/bot/inline.py
+++ AnonXMusic/plugins/bot/inline.py
@@ -43,14 +43,14 @@ async def inline_query_handler(client, query):
                 ]
             )
             searched_text = f"""
-❖ <b>ᴛɪᴛʟᴇ ➥</b> <a href={link}>{title}</a>
+❖ <b>ᴛɪᴛʟᴇ ~</b> <a href={link}>{title}</a>
 
-● <b>ᴅᴜʀᴀᴛɪᴏɴ ➥</b> {duration} ᴍɪɴᴜᴛᴇs
-● <b>ᴠɪᴇᴡs ➥</b> <code>{views}</code>
-● <b>ᴄʜᴀɴɴᴇʟ ➥</b> <a href={channellink}>{channel}</a>
-● <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ ➥</b> {published}
+● <b>ᴅᴜʀᴀᴛɪᴏɴ ~</b> {duration} ᴍɪɴᴜᴛᴇs
+● <b>ᴠɪᴇᴡs ~</b> <code>{views}</code>
+● <b>ᴄʜᴀɴɴᴇʟ ~</b> <a href={channellink}>{channel}</a>
+● <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ ~</b> {published}
 
-<b>❖ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ ➥ {app.name}</b>"""
+<b>❖ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ ~ {app.name}</b>"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
