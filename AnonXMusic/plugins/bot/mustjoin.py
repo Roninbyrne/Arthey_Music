21c764509f5557d04362bbfa7ed65f02a06f9a88
Ronin <bhabyaprakash01@gmail.com>
2024-08-15 18:55:11 +0530

    Update mustjoin.py

--- AnonXMusic/plugins/bot/mustjoin.py
+++ AnonXMusic/plugins/bot/mustjoin.py
@@ -47,7 +47,7 @@ async def must_join_channel(app: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/babaaasaaa"),
+                                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/arona_update"),
                                 InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=link),
                             ]
                         ]
