e1a5d1d4bb82ef9d2e32f77cc47e8e3f68a08123
Ronin <bhabyaprakash01@gmail.com>
2024-08-15 12:04:24 +0530

    Update groupinfo.py

--- AnonXMusic/plugins/tools/groupinfo.py
+++ AnonXMusic/plugins/tools/groupinfo.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Arthey_bot?startgroup=true"),
     ],
 ]
 
@@ -34,7 +34,7 @@ async def get_group_status(_, message: Message):
         f"● ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs ➥ {total_members}\n"
         f"● ᴜsᴇʀɴᴀᴍᴇ ➥ @{group_username}\n"
         f"● ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ➥ \n{group_description or 'N/A'}\n\n"
-        f"● ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐"
+        f"● ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ 𝐀𝐫𝐭𝐡𝐞𝐲"
     )
     
     await message.reply((response_text),reply_markup=InlineKeyboardMarkup(EVAA),)
