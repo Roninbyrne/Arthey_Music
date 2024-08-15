545d66f519bef49531a3b95780d8c613dd892de8
Ronin <bhabyaprakash01@gmail.com>
2024-08-15 12:09:09 +0530

    Update telegraph.py

--- AnonXMusic/plugins/tools/telegraph.py
+++ AnonXMusic/plugins/tools/telegraph.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Arthey_bot?startgroup=true"),
     ],
 ]
 
