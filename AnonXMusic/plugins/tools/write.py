f8b88e5d9cc70576f3188d592379b44c0dfab7f8
Ronin <bhabyaprakash01@gmail.com>
2024-08-15 12:01:48 +0530

    Update write.py

--- AnonXMusic/plugins/tools/write.py
+++ AnonXMusic/plugins/tools/write.py
@@ -6,7 +6,7 @@ import requests
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Arthey_bot?startgroup=true"),
     ],
 ]
 
@@ -19,7 +19,7 @@ async def handwrite(_, message: Message):
     m =await message.reply_text( "📝")
     write = requests.get(f"https://apis.xditya.me/write?text={text}").url
 
-    caption = f"""❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ɴ ʏ ᴋ ᴀ ᴀ ࿐"""
+    caption = f"""❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ~ 𝐀𝐫𝐭𝐡𝐞𝐲"""
     await m.delete()
     await message.reply_photo(photo=write,caption=caption, reply_markup=InlineKeyboardMarkup(EVAA),)
   
