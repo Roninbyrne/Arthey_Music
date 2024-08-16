896c4554186f5cfe9209ccc4431677f56d21df1b
Ronin <bhabyaprakash01@gmail.com>
2024-08-16 09:09:38 +0530

    Update wish.py

--- AnonXMusic/plugins/tools/wish.py
+++ AnonXMusic/plugins/tools/wish.py
@@ -4,7 +4,7 @@ import random
 import requests
 from AnonXMusic import app 
 
-SUPPORT_CHAT = "The_friendz"
+SUPPORT_CHAT = "PhoenixXsupport"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
