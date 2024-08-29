b05d4d0e6ffefd354cdfcd7bdb670a74611420a4
Ronin <bhabyaprakash01@gmail.com>
2024-08-29 22:30:23 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="AnonXAss1",
+            name="MukeshXAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="AnonXAss2",
+            name="MukeshXAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="AnonXAss3",
+            name="MukeshXAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="AnonXAss4",
+            name="MukeshXAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="AnonXAss5",
+            name="MukeshXAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -47,38 +47,38 @@ class Userbot(Client):
         )
 
     async def start(self):
-        LOGGER(__name__).info(f"❖ Starting Assistants...")
+        LOGGER(__name__).info(f"Starting Assistants...")
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("roy_editx")
-                await self.one.join_chat("choti_bachii")
+                await self.one.join_chat("Grabber_Society")
+                await self.one.join_chat("Grabber_memes")
             except:
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "❖ Assistant Started")
+                await self.one.send_message(config.LOGGER_ID, "Assistant Started")
             except:
                 LOGGER(__name__).error(
-                    "❖ Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                    "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
                 )
                 exit()
             self.one.id = self.one.me.id
             self.one.name = self.one.me.mention
             self.one.username = self.one.me.username
             assistantids.append(self.one.id)
-            LOGGER(__name__).info(f"❖ Assistant Started as  {self.one.name}")
+            LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
 
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("phoenixXsupport")
-                await self.one.join_chat("Grabber_memes")
+                await self.two.join_chat("Grabber_Society")
+                await self.two.join_chat("Grabber_memes")
             except:
                 pass
             assistants.append(2)
             try:
-                await self.two.send_message(config.LOGGER_ID, "❖ Assistant Started")
+                await self.two.send_message(config.LOGGER_ID, "Assistant Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -88,18 +88,18 @@ class Userbot(Client):
             self.two.name = self.two.me.mention
             self.two.username = self.two.me.username
             assistantids.append(self.two.id)
-            LOGGER(__name__).info(f"❖ Assistant Two Started as ➥ {self.two.name}")
+            LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
 
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("roy_editx")
-                await self.one.join_chat("choti_bachii")
+                await self.three.join_chat("Grabber_Society")
+                await self.three.join_chat("Grabber_memes")
             except:
                 pass
             assistants.append(3)
             try:
-                await self.three.send_message(config.LOGGER_ID, "❖ Assistant Started")
+                await self.three.send_message(config.LOGGER_ID, "Assistant Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -109,13 +109,13 @@ class Userbot(Client):
             self.three.name = self.three.me.mention
             self.three.username = self.three.me.username
             assistantids.append(self.three.id)
-            LOGGER(__name__).info(f"❖ Assistant Three Started as ➥ {self.three.name}")
+            LOGGER(__name__).info(f"Assistant Three Started as {self.three.name}")
 
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("roy_editx")
-                await self.one.join_chat("choti_bachii")
+                await self.four.join_chat("Grabber_Society")
+                await self.four.join_chat("Grabber_memes")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("roy_editx")
-                await self.one.join_chat("choti_bachii")
+                await self.five.join_chat("Grabber_Society")
+                await self.five.join_chat("Grabber_memes")
             except:
                 pass
             assistants.append(5)
@@ -154,7 +154,7 @@ class Userbot(Client):
             LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}")
 
     async def stop(self):
-        LOGGER(__name__).info(f"❖ Stopping Assistants...")
+        LOGGER(__name__).info(f"Stopping Assistants...")
         try:
             if config.STRING1:
                 await self.one.stop()
